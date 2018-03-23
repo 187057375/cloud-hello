76dd3ad0cce48450103aae040875c783f5fbd8ae
187057375@qq.com <187057375@qq.com>
2018-03-23 16:29:20 +0800

    idgenarator

---
+++ src/main/java/com/mycompany/cloud/controller/test/zookeeper/idgenarator/IdGenarator.java
@@ -0,0 +1,150 @@
+package com.mycompany.cloud.controller.test.zookeeper.idgenarator;
+
+import org.I0Itec.zkclient.ZkClient;
+import org.I0Itec.zkclient.exception.ZkNodeExistsException;
+import org.I0Itec.zkclient.serialize.BytesPushThroughSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author peter
+ * @version V1.0 创建时间：18/3/23
+ * Zookeeper的命名服务(ID生成器)
+ */
+public class IdGenarator {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private ZkClient client = null;
+    // 服务地址
+    private final String server;
+    // id生成器根节点
+    private final String root;
+    // id节点
+    private final String nodeName;
+    // 启动状态: true:启动;false:没有启动，默认没有启动
+    private volatile boolean running = false;
+    private ExecutorService cleanExector = null;
+
+    public enum RemoveMethod {
+        // 不，立即，延期
+        NONE, IMMEDIATELY, DELAY
+    }
+
+    public IdGenarator(String zkServer, String root, String nodeName) {
+        this.server = zkServer;
+        this.root = root;
+        this.nodeName = nodeName;
+    }
+
+    /**
+     * 启动
+     */
+    public void start() throws Exception {
+        if (running)
+            throw new Exception("server has stated...");
+        running = true;
+        init();
+    }
+
+    /**
+     * 停止服务
+     */
+    public void stop() throws Exception {
+        if (!running)
+            throw new Exception("server has stopped...");
+        running = false;
+        freeResource();
+    }
+
+    private void init() {
+        client = new ZkClient(server, 5000, 5000, new BytesPushThroughSerializer());
+        cleanExector = Executors.newFixedThreadPool(10);
+        try {
+            client.createPersistent(root, true);
+        } catch (ZkNodeExistsException e) {
+            logger.info("节点已经存在,节点路径:" + root);
+        }
+
+    }
+
+    /**
+     * 资源释放 T
+     */
+    private void freeResource() {
+        cleanExector.shutdown();
+        try {
+            cleanExector.awaitTermination(2, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            cleanExector = null;
+        }
+        if (client != null) {
+            client.close();
+            client = null;
+        }
+    }
+
+    /**
+     * 判断是否启动服务
+     */
+    private void checkRunning() throws Exception {
+        if (!running)
+            throw new Exception("请先调用start启动服务");
+    }
+
+    /**
+     * 提取ID
+     */
+    private String ExtractId(String str) {
+        int index = str.lastIndexOf(nodeName);// 20
+        if (index >= 0) {
+            index += nodeName.length();
+            return index <= str.length() ? str.substring(index) : "";
+        }
+        return str;
+    }
+
+    /**
+     * 获取id
+     */
+    public String generateId(RemoveMethod removeMethod) throws Exception {
+        checkRunning();
+        final String fullNodePath = root.concat("/").concat(nodeName);
+        // 创建顺序节点每个父节点会为他的第一级子节点维护一份时序，会记录每个子节点创建的先后顺序。
+        // 基于这个特性，在创建子节点的时候，可以设置这个属性，那么在创建节点过程中，
+        // ZooKeeper会自动为给定节点名加上一个数字后缀，作为新的节点名
+        final String ourPath = client.createPersistentSequential(fullNodePath, null);
+        if (removeMethod.equals(RemoveMethod.IMMEDIATELY)) {// 立即删除
+            client.delete(ourPath);
+        } else if (removeMethod.equals(RemoveMethod.DELAY)) {// 延期删除
+            cleanExector.execute(new Runnable() {
+                public void run() {
+                    client.delete(ourPath);
+                }
+            });
+
+        }
+        return ExtractId(ourPath);
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        IdGenarator idGenarator = new IdGenarator("127.0.0.1:2181","/NameService/IdGen", "ID-");
+        idGenarator.start();
+
+        try {
+            for (int i = 0; i < 2; i++) {
+                String id = idGenarator.generateId(RemoveMethod.DELAY);
+                System.out.println(id);
+            }
+        } finally {
+            idGenarator.stop();
+        }
+    }
+
+}
